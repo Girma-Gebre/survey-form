27d2e5df7e22fd5ff0b95c53ff72a5fa6b1ea569
Girma Gebre <girmagebre555@gmail.com>
2026-04-09 14:42:37 +0300

    add backend url for the 2nd time

--- frontend/script.js
+++ frontend/script.js
@@ -66,7 +66,7 @@ document.getElementById("survey-form").addEventListener("submit", async function
     // collecting data from html input element
     const sendData = { name, email, age, employ, likeWeb, tools, comment }; // using ES6 shorthand object syntax.
     // const url = "http://localhost:2000/surveyForm"; // this url is for local database managment 
-    const url = "https://survey-form-back-end.onrender.com/surveyForm"; 
+    const url = "https://survey-form-rrq0.onrender.com/surveyForm"; 
     //this url is for cloud database managment in mongodb atlas and render server
     const endpointObject = {
       method: "POST", 
