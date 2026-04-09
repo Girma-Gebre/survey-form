996a286d97bb81a4aa9ba34f051743a53f8c8de8
Girma Gebre <girmagebre555@gmail.com>
2026-04-09 16:17:06 +0300

    add .gitignore

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,13 @@ const cors = require('cors');
 const express = require('express');
 const postRouter = require('./crude operation/router-post');
 const path = require('path');
-const APP = express(); 
- 
+const APP = express();  
+APP.use(express.static(path.join(__dirname, "../frontend"))); // keeping files in the public folder and the html file must be named as "index.html" for local and cloud base management
 const port = 2000
 APP.use(cors())
 // APP.use(express.static); // keeping files in the public folder for local
 APP.use(express.json()); // to handle the json() caming data
-APP.use(express.urlencoded({extended: true})); 
-APP.use(express.static(path.join(__dirname, "../frontend"))); // keeping files in the public folder and the html file must be named as "index.html" for local and cloud base management
+APP.use(express.urlencoded({extended: true}));
 APP.use('/',postRouter); // enable the router to run
 
 APP.listen(process.env.PORT || port,()=>{
--- frontend/script.js
+++ frontend/script.js
@@ -66,7 +66,7 @@ document.getElementById("survey-form").addEventListener("submit", async function
     // collecting data from html input element
     const sendData = { name, email, age, employ, likeWeb, tools, comment }; // using ES6 shorthand object syntax.
     // const url = "http://localhost:2000/surveyForm"; // this url is for local database managment 
-    const url = "https://survey-form-rrq0.onrender.com/surveyform"; 
+    const url = "https://survey-form-backend-web.onrender.com/surveyform"; 
     //this url is for cloud database managment in mongodb atlas and render server. note: the  url must be in lowercase letter 
     const endpointObject = {
       method: "POST", 
